6e2f3e52ba82269275ebf7a8a123c7f8a4cd796b
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-08-26 19:44:13 +0100

    Add moregradient tests to dist_test, cleaned up code

--- MCMC/HMC/Distributions/src/distribution_test.py
+++ MCMC/HMC/Distributions/src/distribution_test.py
@@ -4,35 +4,80 @@ from torch.autograd import Variable
 import torch
 from HMC.Distributions.src import distributions as dis
 
+mean = torch.rand(3).unsqueeze(-1).t()
+std  = torch.rand(3).unsqueeze(-1).t()
 ## testing Normal
-mean = torch.Tensor([0.5]).unsqueeze(-1)
-std  = torch.Tensor([0.6]).unsqueeze(-1)
-normal_obj = dis.Normal(mean, std)
-sample = normal_obj.sample(num_samples = 1)
-# print(sample)
-# print(normal_obj.logpdf(sample))
+def true_grad_normal(sample, mean, std, diff):
+    if diff == 'sample':
+        return torch.sign(sample - mean) / std
+    elif diff == 'mean':
+        return -torch.sign(sample - mean) / std
+    else:
+        return -2/std + torch.div(torch.abs(sample- mean), std**2)
+
+def testing_normal(mean, std):
+    mean       = Variable(mean, requires_grad = True)
+    std        = Variable(std, requires_grad=True)
+    normal_obj = dis.Normal(mean, std)
+    sample     = normal_obj.sample(num_samples = mean.size()[1])
+    sample     = Variable(sample.data, requires_grad = True)
+    normal_logpdf = normal_obj.logpdf(sample)
+
+    diff_logpdf  = torch.autograd.grad([normal_logpdf], [sample], grad_outputs=torch.ones(sample.size()), retain_graph=True)[0]
+    diff_logpdf2 = torch.autograd.grad([normal_logpdf], [mean], grad_outputs=torch.ones(mean.size()), retain_graph=True)[0]
+    diff_logpdf3 = torch.autograd.grad([normal_logpdf], [std], grad_outputs=torch.ones(std.size()), retain_graph=True)[0]
+
+    print(diff_logpdf)
+    print(diff_logpdf2)
+    print(diff_logpdf3)
+
+
 def true_grad_laplace(sample, loc, scale, diff):
     if diff == 'location':
         return torch.sign(sample - loc) / scale
     elif diff == 'sample':
-        return torch.sign(sample - loc) / scale
+        return -torch.sign(sample - loc) / scale
     else:
-        return -2/scale - torch.abs(sample- loc)
+        return -2/scale + torch.div(torch.abs(sample- loc), scale**2)
 
 
 ## testing Laplace
-location = Variable(mean, requires_grad = True)
-scale    = Variable(std)
-laplace_obj     = dis.Laplace(location, scale)
-sample          = laplace_obj.sample()
-laplace_logpdf  = laplace_obj.logpdf(sample,location,scale)
-diff_logpdf     = torch.autograd.grad([laplace_logpdf], [location])
-print('Sample ', sample)
-print('Laplace logpdf ', laplace_logpdf)
-# print('Diffferential of sample',diff_sample
-print('Differential of logpdf ', diff_logpdf)
-print('True grad wrt to sample', true_grad_laplace(sample,location,scale,diff = 'location'))
+def testing_laplace(mean, std):
+    '''Test the derivatives of the logpdf w.r.t the 'sample'
+     'location (mean)'  and 'scale' (std)
+     input
+     -----
+     mean - Type: Torch.tensors or np.arrays [1, .., N]
+     std  - Type: Torch.tensors or np.arrays [1, .., N]
+     '''
+    location = Variable(mean, requires_grad = True)
+    scale    = Variable(std, requires_grad = True)
+    laplace_obj     = dis.Laplace(location, scale)
+    sample          = Variable(laplace_obj.sample().data, requires_grad = True)
+    laplace_logpdf  = laplace_obj.logpdf(sample)
+    # Spits out the gradient as a tuple, so [0] takes the variable object from the tuple
+    # containing the gradients
+    diff_logpdf     = torch.autograd.grad([laplace_logpdf], [sample], grad_outputs= torch.ones(sample.size()), retain_graph= True)[0]
+    diff_logpdf2    = torch.autograd.grad([laplace_logpdf], [location],grad_outputs= torch.ones(location.size()), retain_graph = True)[0]
+    diff_logpdf3    = torch.autograd.grad([laplace_logpdf], [scale], grad_outputs= torch.ones(scale.size()), retain_graph = True)[0]
+    # Pytorch gradients
+    print('Printing autograd gradients: ')
+    print(diff_logpdf)
+    print(diff_logpdf2)
+    print(diff_logpdf3)
+    # True gradients
+    print()
+    print('Printing true gradients ')
+    print('grad_sample',true_grad_laplace(sample,location,scale,diff = 'sample').data)
+    print('grad_location',true_grad_laplace(sample,location,scale,diff = 'location').data)
+    print('grad_scale', true_grad_laplace(sample, location, scale, diff = 'scale').data)
 
+# Testing categorical
+def testing_categorical()
+# Testing normal
+#testing_normal(mean,std)
+# Testing laplacian
+# testing_laplace(mean,std)
 # Testing categorical
 
 # Testing Bernoulli
