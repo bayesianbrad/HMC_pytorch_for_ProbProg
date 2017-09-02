ee139eb218fedeffd2396f0305e5f02b6269f2bf
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-09-02 23:37:24 +0100

    commiting any unstaged files

--- MCMC/HMC/Tests/src0.py
+++ MCMC/HMC/Tests/src0.py
@@ -2,34 +2,44 @@ import torch
 import numpy as np
 from torch.autograd import Variable
 from z import *
+class program():
+    ''''This needs to be a function of all free variables.
+         If I provide a map of all vlues and computes the log density
+         and assigns values rather than samples.
+         If I don't provide then it samples
+         For any variable values provided in the map, they are assigned
 
-a= VariableCast(1.0)
-b= VariableCast(1.41)
-normal_object = Normal(a, b)
-x = Variable(torch.Tensor([2]),  requires_grad = True)
-# x.detach()
-# x = normal_object.sample()   #sample
-logp_x = normal_object.logpdf(x)
-# logp_x.backward(retain_graph = True)
-# grad1  = x.grad.data
-# print(grad1)
-std   = VariableCast(1.73)
-p_y_g_x = Normal(x, std)
-obs2    = VariableCast(7.0)
-# y22543 = obs2
+         method
+         def eval
 
-logp_y_g_x = p_y_g_x.logpdf(obs2)
-# logp_y_g_x.backward(retain_graph = True)
-# grad2 = x.grad.data
-# print(grad2)
-# x.grad.data.zero_()
-#
-# print(logp_y_g_x.grad_fn)
-logp_x_y = Variable.add(logp_x,logp_y_g_x)
-print(logp_x_y)
-# print(logp_x_y.grad_fn)
-grad_x = torch.autograd.grad(outputs = [logp_x_y], inputs= [x])
-print(grad_x)
+         Needs to be a class '''
+    def __init__(self):
+        '''Generating code, returns  a map of variable names / symbols '''
+        self.params = {'x': None}
+
+    def eval(self, values):
+        ''' Takes a map of variable names, to variable values '''
+        a = VariableCast(1.0)
+        b = VariableCast(1.41)
+        normal_object = Normal(a, b)
+        if values['x'] is not None:
+            x = Variable(values['x'], requires_grad=True)
+        # else:
+        #     x = normal_object.sample()
+        #     x = Variable(x.data, requires_grad = True)
+        logp_x = normal_object.logpdf(x)
+        std = VariableCast(1.73)
+        p_y_g_x = Normal(x, std)
+        obs2 = VariableCast(7.0)
+        logp_y_g_x = p_y_g_x.logpdf(obs2)
+        logp_x_y = Variable.add(logp_x, logp_y_g_x)
+        return logp_x_y, {'x':x.data}
+    def free_vars(self):
+        return self.params
+
+    # print(logp_x_y.grad_fn)
+    grad_x = torch.autograd.grad(outputs = [logp_x_y], inputs= [x])
+    print(grad_x)
 # print(grad1 + grad2)
 # print(x)
 # print(logp_x_y)
@@ -58,3 +68,41 @@ print(grad_x)
 # print(sess.run(x))
 # writer = tf.summary.FileWriter( './Graph_Output/g24048', sess.graph)
 # sess.close()
+
+class programif():
+    ''''This needs to be a function of all free variables.
+         If I provide a map of all vlues and computes the log density
+         and assigns values rather than samples.
+         If I don't provide then it samples
+         For any variable values provided in the map, they are assigned
+
+         method
+         def eval
+
+         Needs to be a class '''
+    def __init__(self):
+        '''Generating code, returns  a map of variable names / symbols '''
+        self.params = {'x': None}
+
+    def eval(self, values):
+        ''' Takes a map of variable names, to variable values '''
+        a = VariableCast(0.0)
+        b = VariableCast(1)
+        normal_object = Normal(a, b)
+        if values['x'] is not None:
+            x = Variable(values['x'], requires_grad=True)
+        # else:
+        #     x = normal_object.sample()
+        #     x = Variable(x.data, requires_grad = True)
+        if torch.gt(x,torch.zeros(x.size()))[0][0]:
+            yttyu
+
+        logp_x = normal_object.logpdf(x)
+        std = VariableCast(1.73)
+        p_y_g_x = Normal(x, std)
+        obs2 = VariableCast(7.0)
+        logp_y_g_x = p_y_g_x.logpdf(obs2)
+        logp_x_y = Variable.add(logp_x, logp_y_g_x)
+        return logp_x_y, {'x':x.data}
+    def free_vars(self):
+        return self.params
--- MCMC/HMC/Tests/test_foppl_extracter.py
+++ MCMC/HMC/Tests/test_foppl_extracter.py
@@ -20,3 +20,22 @@ for k in vars.keys():
                 var[kk][k].append(temp)
         else:
             print() # Deal with distribution parameters here
+
+c24039= VariableCast(1.0)
+c24040= VariableCast(2.0)
+x24041 = Normal(c24039, c24040)
+x22542 = Variable(torch.Tensor([0.0]),  requires_grad = True)
+# x22542.detach()
+# x22542 = x24041.sample()   #sample
+p24042 = x24041.logpdf( x22542)
+c24043 = VariableCast(3.0)
+x24044 = Normal(x22542, c24043)
+c24045 = VariableCast(7.0)
+y22543 = c24045
+p24046 = x24044.logpdf( y22543)
+p24047 = Variable.add(p24042,p24046)
+
+print(x22542)
+print(p24047)
+grad_x22542 = torch.autograd.grad([p24047], [x22542] )
+print('gradient of x22542 ', x22542)
