45349a4046e7dd3facf9e381d324f59d1b137b6a
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-09-07 14:06:11 +0100

    both HMCsampler and plotting take additional argument model

--- MCMC/HMCSampler/HMC/main.py
+++ MCMC/HMCSampler/HMC/main.py
@@ -9,7 +9,8 @@ License: MIT
 '''
 from HMC.hmc_class_general import HMCsampler as HMC
 from Utils.plotting_and_saving import Plotting
-hmcsampler  = HMC(burn_in=100, n_samples = 1000)
+models = ['conjgauss', 'conditionalif', 'linearreg']
+hmcsampler  = HMC(burn_in=100, n_samples = 10000, model= models[1])
 samples, samples_with_burnin, mean =  hmcsampler.run_sampler()
-plots = Plotting(samples,samples_with_burnin,mean)
+plots = Plotting(samples,samples_with_burnin,mean, model = models[1])
 plots.call_all_methods()
