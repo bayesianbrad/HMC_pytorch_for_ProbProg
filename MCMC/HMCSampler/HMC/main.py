6ecb7f413532ab7b38b4f96cf130aa82f85051cb
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-09-08 23:28:42 +0100

    Just running different experiments

--- MCMC/HMCSampler/HMC/main.py
+++ MCMC/HMCSampler/HMC/main.py
@@ -10,7 +10,7 @@ License: MIT
 from HMC.hmc_class_development import HMCsampler as HMC
 from Utils.plotting_and_saving import Plotting
 models = ['conjgauss', 'conditionalif', 'linearreg']
-hmcsampler  = HMC(burn_in=0, n_samples = 2000, dim=1, model= models[0])
+hmcsampler  = HMC(burn_in=300, n_samples = 1000, model= models[1])
 samples, samples_with_burnin, mean =  hmcsampler.run_sampler()
-plots = Plotting(samples,samples_with_burnin,mean, model = models[0])
+plots = Plotting(samples,samples_with_burnin,mean, model = models[1])
 plots.call_all_methods()
