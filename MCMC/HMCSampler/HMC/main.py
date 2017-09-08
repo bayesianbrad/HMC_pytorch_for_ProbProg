0c0324e64230ba315a8f0f62e2acd50b29213a8b
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-09-08 17:01:19 +0100

    Added dim input , will be taken away in the future

--- MCMC/HMCSampler/HMC/main.py
+++ MCMC/HMCSampler/HMC/main.py
@@ -7,10 +7,10 @@ Date created:  06/09/2017
 
 License: MIT
 '''
-from HMC.hmc_class_general import HMCsampler as HMC
+from HMC.hmc_class_development import HMCsampler as HMC
 from Utils.plotting_and_saving import Plotting
 models = ['conjgauss', 'conditionalif', 'linearreg']
-hmcsampler  = HMC(burn_in=100, n_samples = 10000, model= models[1])
+hmcsampler  = HMC(burn_in=0, n_samples = 1000, dim=2, model= models[2])
 samples, samples_with_burnin, mean =  hmcsampler.run_sampler()
-plots = Plotting(samples,samples_with_burnin,mean, model = models[1])
+plots = Plotting(samples,samples_with_burnin,mean, model = models[2])
 plots.call_all_methods()
