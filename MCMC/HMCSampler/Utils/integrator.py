4002c4754845122af29a250208c23f8f533c2506
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-09-08 17:04:03 +0100

    Now caclualtes the trajectory length and step size

--- MCMC/HMCSampler/Utils/integrator.py
+++ MCMC/HMCSampler/Utils/integrator.py
@@ -8,27 +8,47 @@ Date created:  06/09/2017
 License: MIT
 '''
 import torch
+import numpy as np
 from Utils.kinetic import Kinetic
 from torch.autograd import Variable
 
 class Integrator():
 
-    def __init__(self, potential, step_size = 0.03, traj_size = 10):
-        self.potential   = potential
-        self.step_size   = step_size
-        self.traj_size   = traj_size
+    def __init__(self, potential, min_step, max_step, max_traj, min_traj):
+        self.potential = potential
+        if min_step is None:
+            self.min_step = np.random.uniform(0.01, 0.07)
+        else:
+            self.min_step = min_step
+        if max_step is None:
+            self.max_step = np.random.uniform(0.07, 0.18)
+        else:
+            self.max_step = max_step
+        if max_traj is None:
+            self.max_traj = np.random.uniform(18, 25)
+        else:
+            self.max_traj = max_traj
+        if min_traj is None:
+            self.min_traj = np.random.uniform(1, 18)
+        else:
+            self.min_traj = min_traj
+    # def list_to_tensor(self, params):
+    #     ''' Unpacks the parameters list tensors and converts it to list
+    #
+    #     returns tensor of  num_rows = len(values) and num_cols  = 1
+    #     problem:
+    #         if there are col dimensions greater than 1, then this will not work'''
+    #     assert(isinstance(params, list))
+    #     temp = Variable(torch.Tensor(len(params)).unsqueeze(-1))
+    #     for i in range(len(params)):
+    #         temp[i,:] = params[i]
+    #     return temp
+    def generate_new_step_traj(self):
+        ''' Generates a new step adn trajectory size  '''
+        step_size = np.random.uniform(self.min_step, self.max_step)
+        traj_size = int(np.random.uniform(self.min_traj, self.max_traj))
+        return step_size, traj_size
 
-    def list_to_tensor(self, params):
-        ''' Unpacks the parameters list tensors and converts it to list
-
-        returns tensor of  num_rows = len(values) and num_cols  = 1
-        problem:
-            if there are col dimensions greater than 1, then this will not work'''
-        assert(isinstance(params, list))
-        temp = Variable(torch.Tensor(len(params)).unsqueeze(-1))
-        for i in range(len(params)):
-            temp[i,:] = params[i]
-        return temp
 
     def leapfrog(self, p_init, values, grad_init):
         '''Performs the leapfrog steps of the HMC for the specified trajectory
@@ -37,7 +57,6 @@ class Integrator():
         ----------
             values_init
             p_init
-            logjoint_init
             grad_init     - Description: contains the initial gradients of the joint w.r.t parameters.
 
         Outputs
@@ -45,20 +64,24 @@ class Integrator():
             values -    Description: proposed new values
             p      -    Description: proposed new auxillary momentum
         '''
-
-        values_init = self.list_to_tensor(values)
+        step_size, traj_size = self.generate_new_step_traj()
+        values_init = values
         self.kinetic = Kinetic(p_init)
         # Start by updating the momentum a half-step and values by a full step
-        p = p_init + 0.5 * self.step_size * grad_init
-        values = values_init + self.step_size * self.kinetic.gauss_ke(p, grad=True)
-        for i in range(self.traj_size - 1):
+        p = p_init + 0.5 * step_size * grad_init
+        values = values_init + step_size * self.kinetic.gauss_ke(p, grad=True)
+        for i in range(traj_size - 1):
             # range equiv to [2:nsteps] as we have already performed the first step
             # update momentum
-            p = p + self.step_size * self.potential.eval(values, grad=True)
+            print('Debug: Location: integrator.leapfrog Value: p size and p value', p.data.size(), p)
+            print('Debug: Location: integrator.leapfrog Value: step_size' ,step_size)
+            p = p + step_size * self.potential.eval(values, grad=True)
+            print('Debug: Location: integrator.leapfrog Value: the first half step has succeeded ')
             # update values
-            values = values + self.step_size * self.kinetic.gauss_ke(p, grad=True)
+            values = values + step_size * self.kinetic.gauss_ke(p, grad=True)
+            print('')
 
         # Do a final update of the momentum for a half step
-        p = p + 0.5 * self.step_size * self.potential.eval(values, grad=True)
+        p = p + 0.5 * step_size * self.potential.eval(values, grad=True)
         # return new proposal state
         return values, p
