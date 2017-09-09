b803b1d68c6144e6c190cf22cc09059bcfc50ffa
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-09-09 14:19:41 +0100

    Added basic for loop to run all the models

--- MCMC/HMCSampler/HMC/main.py
+++ MCMC/HMCSampler/HMC/main.py
@@ -10,7 +10,8 @@ License: MIT
 from HMC.hmc_class_development import HMCsampler as HMC
 from Utils.plotting_and_saving import Plotting
 models = ['conjgauss', 'conditionalif', 'linearreg']
-hmcsampler  = HMC(burn_in=300, n_samples = 1000, model= models[1])
-samples, samples_with_burnin, mean =  hmcsampler.run_sampler()
-plots = Plotting(samples,samples_with_burnin,mean, model = models[1])
-plots.call_all_methods()
+for i in range(len(models)):
+    hmcsampler  = HMC(burn_in=0, n_samples = 5000, model= models[i])
+    samples, samples_with_burnin, mean =  hmcsampler.run_sampler()
+    plots = Plotting(samples,samples_with_burnin,mean, model = models[i])
+    plots.call_all_methods()
