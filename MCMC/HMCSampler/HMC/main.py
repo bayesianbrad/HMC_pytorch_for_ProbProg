54156f7b812789b70ccc38de4b76d01123b0493d
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-09-10 16:38:38 +0100

    Minor change to imports

--- MCMC/HMCSampler/HMC/main.py
+++ MCMC/HMCSampler/HMC/main.py
@@ -7,16 +7,16 @@ Date created:  06/09/2017
 
 License: MIT
 '''
-from HMC.hmc_class_development import HMCsampler as HMC
+from HMC.hmc_class import HMCsampler as HMC
 from Utils.plotting_and_saving import Plotting
-models = ['conjgauss', 'conditionalif', 'linearreg']
-i = 2
+models = ['conjgauss', 'conditionalif', 'linearreg', 'hierarchial', 'mixture']
+i = 3
 # for i in range(len(models)):
-#     hmcsampler  = HMC(burn_in=0, n_samples = 5000, model= models[i])
+#     hmcsampler  = HMC(burn_in=0, n_samples = 500, model= models[i])
 #     samples, samples_with_burnin, mean =  hmcsampler.run_sampler()
 #     plots = Plotting(samples,samples_with_burnin,mean, model = models[i])
-#     plots.call_all_methods()    hmcsampler  = HMC(burn_in=0, n_samples = 5000, model= models[i])
-hmcsampler  = HMC(burn_in=10, n_samples = 1000, model= models[i])
+#     plots.call_all_methods()
+hmcsampler  = HMC(burn_in=0, n_samples = 1000, model= models[i])
 samples_with_burnin,samples, mean =  hmcsampler.run_sampler()
 plots = Plotting(samples,samples_with_burnin,mean, model = models[i])
 plots.call_all_methods()
