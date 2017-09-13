320684d6b15c68ae80a7b6279e901e89a30372df
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2017-09-13 12:26:40 +0100

    cleaning up deleted files

--- MCMC/HMCSampler/HMC/main.py
+++ MCMC/HMCSampler/HMC/main.py
@@ -11,12 +11,12 @@ from HMC.hmc_class import HMCsampler as HMC
 from Utils.plotting_and_saving import Plotting
 models = ['conjgauss', 'conditionalif', 'linearreg', 'hierarchial', 'mixture']
 i = 1
-for i in range(len(models)-2):
-    hmcsampler  = HMC(burn_in=0, n_samples = 5000, model= models[i])
-    samples, samples_with_burnin, mean =  hmcsampler.run_sampler()
-    plots = Plotting(samples,samples_with_burnin,mean, model = models[i])
-    plots.call_all_methods()
-# hmcsampler  = HMC(burn_in=0, n_samples = 5000, model= models[i])
-# samples_with_burnin,samples, mean =  hmcsampler.run_sampler()
-# plots = Plotting(samples,samples_with_burnin,mean, model = models[i])
-# plots.call_all_methods()
+# for i in range(len(models)-2):
+#     hmcsampler  = HMC(burn_in=0, n_samples = 5000, model= models[i])
+#     samples, samples_with_burnin, mean =  hmcsampler.run_sampler()
+#     plots = Plotting(samples,samples_with_burnin,mean, model = models[i])
+#     plots.call_all_methods()
+hmcsampler  = HMC(burn_in=0, n_samples = 5000, model= models[i])
+samples_with_burnin,samples, mean =  hmcsampler.run_sampler()
+plots = Plotting(samples,samples_with_burnin,mean, model = models[i])
+plots.call_all_methods()
